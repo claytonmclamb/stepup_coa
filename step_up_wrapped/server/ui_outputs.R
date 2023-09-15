1e6723b00bd721a7f154d155724361ec183401e2
claytonmclamb <cmclamb@elon.edu>
2023-09-15 11:43:32 -0400

    button showing + changed colors

--- step_up_wrapped/server/ui_outputs.R
+++ step_up_wrapped/server/ui_outputs.R
@@ -1,8 +1,12 @@
 output$alltime_grid_ui <- renderUI({
   if (clicks_1$n == 0) {
     clicks_1$n <- 1
-    actionButton("at_grid_button", label = "Reveal artists")
+    actionButton("at_grid_button", label = "Reveal Donors")
   } else {
     HTML('<button id="at_grid_button" type="button" class="btn btn-default action-button">Reveal artists</button> <div id="alltime_grid_html" class="shiny-html-output"></div>')
   }
 })
+
+clicks_1 <- reactiveValues(
+  n = 0
+)
